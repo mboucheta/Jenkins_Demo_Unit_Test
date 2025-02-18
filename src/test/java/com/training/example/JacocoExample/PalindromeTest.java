1261ba91fdedfc61e18d13da59a35c889060505d
Mohamed Boucheta <mohamed.boucheta@sncf.fr>
2025-02-18 14:31:59 +0100

    Update PalindromeTest.java

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -11,4 +11,10 @@ public class PalindromeTest {
 	    Palindrome palindromeTester = new Palindrome();
 	    assertFalse(palindromeTester.isPalindrome("madame"));
 	}
+	@Test
+	public void whenEmptyString_thenAccept() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome("madam"));
+	}
+	
 }
